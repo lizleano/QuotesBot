4384a651684b172f516a63e516726a9f994ae208
lizleano <lizleano35@gmail.com>
2017-09-16 15:39:42 -0400

    Corections uploaded

--- TweetingHappiness.py
+++ TweetingHappiness.py
@@ -19,30 +19,32 @@ happy_quotes = [
     # "Happiness is a warm puppy. - Charles M. Schulz",
     # "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
     # "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"
-    'If you want to achieve greatness stop asking for permission. ~Anonymous',
-	'Things work out best for those who make the best of how things work out. ~John Wooden',
-	'To live a creative life, we must lose our fear of being wrong. ~Anonymous',
-	'If you are not willing to risk the usual you will have to settle for the ordinary. ~Jim Rohn',
-	'Trust because you are willing to accept the risk, not because it’s safe or certain. ~Anonymous',
-	'Take up one idea. Make that one idea your life - think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success. ~Swami Vivekananda',
-	'All our dreams can come true if we have the courage to pursue them. ~Walt Disney',
-	'Good things come to people who wait, but better things come to those who go out and get them. ~Anonymous',
+ #    'If you want to achieve greatness stop asking for permission. ~Anonymous',
+	# 'Things work out best for those who make the best of how things work out. ~John Wooden',
+	# 'To live a creative life, we must lose our fear of being wrong. ~Anonymous',
+	# 'If you are not willing to risk the usual you will have to settle for the ordinary. ~Jim Rohn',
+	# 'Trust because you are willing to accept the risk, not because it’s safe or certain. ~Anonymous',
+	# 'Take up one idea. Make that one idea your life - think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body, be full of that idea, and just leave every other idea alone. This is the way to success. ~Swami Vivekananda',
+	# 'All our dreams can come true if we have the courage to pursue them. ~Walt Disney',
+	# 'Good things come to people who wait, but better things come to those who go out and get them. ~Anonymous',
 	'If you do what you always did, you will get what you always got. ~Anonymous']
 
 # Create function for tweeting
 
 # Create a function that tweets
-def TweetOut(tweet):
+def TweetOut(tweets):
 	try:
 		# Twitter credentials
 		# Setup Tweepy API Authentication
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 		auth.set_access_token(access_token, access_token_secret)
 		api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-		api.update_status(tweets)   
+
+		tweet = random.choice(tweets)
+		api.update_status(tweet)   
 
 		# Print success message
-		print("I twitted: %s" % strTweet)
+		print("I twitted: %s" % tweet)
 
 	except:
 		print("Duplicate")
@@ -50,8 +52,8 @@ def TweetOut(tweet):
 
 # Tweet a random quote
 
-while (True):
+while(True):
 	TweetOut(happy_quotes)
 
 # Set timer to run every minute
-	time.sleep(60)
+	time.sleep(10)
